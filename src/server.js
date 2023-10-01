84470729a4f5f8ca1c743e02f6ae04574e8eda31
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-10-01 19:11:47 -0400

    clean it up

--- src/server.js
+++ src/server.js
@@ -1,107 +1,30 @@
-/**
- * The core server that runs on a Cloudflare worker.
- */
-
 import { json, error, Router } from 'itty-router'
-import * as commands from './commands.js'
 import { middleware } from './middleware.js'
-import { getStars } from './stars.js'
-import {
-    InteractionResponseType,
-    InteractionType,
-    InteractionResponseFlags,
-} from 'discord-interactions'
-import { sleep, discord } from './helpers.js'
+import { getStars, updateChannels } from './stars.js'
 
 const router = Router()
     .all('*', ...middleware)
     .get('/', (request, env) => {
-        return { hello: `👋 ${env.DISCORD_APPLICATION_ID}` }
+        return { '👋': `${env.DISCORD_APPLICATION_ID}` }
     })
-
-router.post('/message/:channel_id', async (request) => {
-    const id = request.params.channel_id
-    return request.discord('POST', `channels/${id}/messages`, request.content)
-})
-
-/**
- * A simple :wave: hello page to verify the worker is working.
- */
-
-/**
- * Main route for all requests sent from Discord.  All incoming messages will
- * include a JSON payload described here:
- * https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
- */
-router.post('/', async (request, env) => {
-    const { isValid, interaction } = await request.verifyDiscordRequest()
-    if (!isValid || !interaction) {
-        return new Response('Bad request signature.', { status: 401 })
-    }
-
-    if (interaction.type === InteractionType.PING) {
-        // The `PING` message is used during the initial webhook handshake, and is
-        // required to configure the webhook in the developer portal.
-        return {
-            type: InteractionResponseType.PONG,
-        }
-    }
-
-    if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-        // Most user commands will come as `APPLICATION_COMMAND`.
-        switch (interaction.data.name.toLowerCase()) {
-            case commands.INVITE_COMMAND.name.toLowerCase(): {
-                const applicationId = env.DISCORD_APPLICATION_ID
-                const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`
-                return {
-                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                    data: {
-                        content: INVITE_URL,
-                        flags: InteractionResponseFlags.EPHEMERAL,
-                    },
-                }
-            }
-            default:
-                return error(400, 'unknown type')
-        }
-    }
-
-    console.error('Unknown Type')
-    return error(400, 'unknown type')
-})
-
-router.all('*', () => new Response('Not Found.', { status: 404 }))
+    .post('/message/:channel_id', async (request) => {
+        const id = request.params.channel_id
+        return request.discord(
+            'POST',
+            `channels/${id}/messages`,
+            request.content,
+        )
+    })
+    .all('/test', () => getStars())
+    .all('*', () => new Response('Not Found.', { status: 404 }))
 
 const server = {
     fetch: (request, env, ctx) =>
         router.handle(request, env, ctx).then(json).catch(error),
     scheduled: async (event, env) => {
-        console.log('start of scheduled')
-        const channels = ['1136705165555159060', '1157344633932763196']
-        const stars = await getStars()
-        const starlines = stars.match(/(?=[\s\S])(?:.*\n?){1,20}/g) // split every 20th new line
-        console.log(starlines)
-        for await (const channel of channels) {
-            const response = await discord(env)(
-                'GET',
-                `channels/${channel}/messages?limit=100`,
-            )
-            const messages = await response.json()
-            for await (const message of messages) {
-                const response = await discord(env)(
-                    'DELETE',
-                    `channels/${channel}/messages/${message.id}`,
-                )
-                console.log(response.status)
-                await sleep(500)
-            }
-
-            for (const chunk of starlines) {
-                const body = { content: `\`\`\`${chunk}\`\`\`` }
-                await discord(env)('POST', `channels/${channel}/messages`, body)
-            }
-        }
-        console.log('end of scheduled')
+        const channels = env.channels
+        console.log(channels)
+        await updateChannels(env, channels)
     },
 }
 
--- src/stars.js
+++ src/stars.js
@@ -1,8 +1,15 @@
+import { sleep, discord } from './helpers.js'
+import { table, getBorderCharacters } from 'table'
 import * as timeago from 'timeago.js'
 import { en_short as locale_en_short } from 'timeago.js/lib/lang/index.js'
-import { table, getBorderCharacters } from 'table'
+
 timeago.register('en_short', locale_en_short)
 
+const api = 'https://map.starminers.site/data'
+
+// these are are from StarMiners, but they're different times than the ones on the wiki:
+// https://oldschool.runescape.wiki/w/Shooting_Stars#Star_sizes
+// consider instead 18+18.42+13.16+8.52+8.75+6+4.62+6+3.21?
 const minutesUntilDead = {
     0: 0,
     1: 24,
@@ -16,40 +23,105 @@ const minutesUntilDead = {
     9: 99,
 }
 
+// yoinked from StarMiners, just cleaned up a bit
+// not entirely sure how this works:
+// - how does summing up diffs tells us anything about the next wave?
+// - magic numbers 90 and 120?
+const calculateNextWave = (stars) => {
+    if (stars.length === 0) return 'N/A'
+    const min = stars.reduce((a, b) => (a.min < b.min ? a : b)).min
+    const max = stars.reduce((a, b) => (a.max > b.max ? a : b)).max
+    const diff = (max - min) / 60
+    if (diff > 90) return 'NEW ROCKS NOW'
+
+    const now = Date.now() / 1000 // in seconds
+    const diffSum = stars.reduce((acc, a) => acc + (now - a.max), 0) / 60
+    const diffAvg = diffSum / stars.length
+    const estimated = Math.round(120 - diffAvg)
+    return `Next wave in approximately ${estimated} minutes`
+}
+
 export const getStars = async () => {
-    const response = await fetch('https://map.starminers.site/data')
+    const now = Date.now() / 1000 // in seconds
+    const response = await fetch(api)
     const stars = (await response.json())
+        .filter((o) => o.minTime !== null && o.maxTime !== null)
+        .map((star) => ({
+            deadtime: star.calledAt + minutesUntilDead[star.tier] * 60,
+            tier: star.tier,
+            world: star.world,
+            location: star.calledLocation.slice(0, 40),
+            called: star.calledAt,
+            calledBy: star.calledBy,
+            min: star.minTime,
+            max: star.maxTime,
+        }))
+        .filter((star) => star.deadtime > now)
+        .sort((a, b) => b.deadtime - a.deadtime || b.tier - a.tier)
         .map((star) => {
-            return {
-                called: star.calledAt * 1000,
-                tier: star.tier,
-                world: star.world,
-                location: star.calledLocation,
-                'called by': star.calledBy,
-                'est. dead':
-                    star.calledAt * 1000 + minutesUntilDead[star.tier] * 60000,
-            }
+            star.called = timeago.format(star.called * 1000, 'en_short')
+            star.deadtime = timeago.format(star.deadtime * 1000, 'en_short')
+            return star
         })
-        .filter((star) => star.tier > 3)
-    stars.sort((a, b) => b.tier - a.tier || b.called - a.called)
-    const formatted = stars.map((star) => {
-        star.called = timeago.format(star.called, 'en_short')
-        star['est. dead'] = timeago.format(star['est. dead'], 'en_short')
-        return star
+    const nextWave = calculateNextWave(stars)
+    const headers = [
+        [nextWave, '', '', '', '', ''],
+        ['Est. Dead', 'Tier', 'World', 'Location', 'Called', 'Called By'],
+    ]
+    const data = stars.map((star) => {
+        delete star.min
+        delete star.max
+        return Object.values(star)
     })
-    return table([Object.keys(stars[0]), ...formatted.map(Object.values)], {
+    return table([...headers, ...data], {
         border: getBorderCharacters('norc'),
+        columns: [
+            { alignment: 'right' },
+            { alignment: 'right' },
+            { alignment: 'right' },
+            { alignment: 'left' },
+            { alignment: 'right' },
+            { alignment: 'left' },
+        ],
+        spanningCells: [{ col: 0, row: 0, colSpan: 6, alignment: 'center' }],
         columnDefault: {
             paddingLeft: 0,
             paddingRight: 2,
         },
         drawHorizontalLine: (lineIndex, rowCount) => {
-            return lineIndex === 1
-            return lineIndex === 0 || lineIndex === rowCount
+            return lineIndex === 1 // lineIndex === 0 || lineIndex === rowCount
         },
         drawVerticalLine: (lineIndex, columnCount) => {
-            return false
-            return lineIndex === 0 || lineIndex === columnCount
+            return false // lineIndex === 0 || lineIndex === columnCount
         },
     })
+}
+
+// 1. deletes all messages in a channel
+// 2. prints stars in chunks of 20 lines
+export const updateChannels = async (env, channels) => {
+    const stars = await getStars()
+    const starlines = stars.match(/(?=[\s\S])(?:.*\n?){1,20}/g) // split every 20th new line
+    console.log(starlines)
+    for await (const channel of channels) {
+        const response = await discord(env)(
+            'GET',
+            `channels/${channel}/messages?limit=100`,
+        )
+        const messages = await response.json()
+        for await (const message of messages) {
+            const response = await discord(env)(
+                'DELETE',
+                `channels/${channel}/messages/${message.id}`,
+            )
+            console.log(response.status)
+            await sleep(500)
+        }
+
+        for (const chunk of starlines) {
+            const body = { content: `\`\`\`${chunk}\`\`\`` }
+            await discord(env)('POST', `channels/${channel}/messages`, body)
+        }
+    }
+    console.log('end of updateChannels')
 }
