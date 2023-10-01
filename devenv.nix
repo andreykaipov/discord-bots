70e204c8e5d792cbda09eb5cae8538e9d59de6f2
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-10-01 09:29:43 -0400

    devenv init

---
+++ devenv.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+{
+  languages.typescript.enable = true;
+  languages.javascript.enable = true;
+  pre-commit.hooks.shellcheck.enable = true;
+
+  packages = with pkgs; [
+    _1password
+    jq
+    entr
+    git
+    wrangler
+    cloudflared
+  ];
+
+  enterShell = ''
+    git --version
+  '';
+}
