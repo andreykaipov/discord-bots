1c937eef7162ff61eb043647bb5c02229ff8a1cf
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-12-30 12:34:42 -0500

    remove copypasta

--- cf/gochujang/src/copypasta.js
+++ cf/gochujang/src/copypasta.js
@@ -1,30 +1,7 @@
 const sanitize = (s) => s.trim().replace(/([^\n])\n([^\n])/g, '$1 $2')
 
 export const thanks = sanitize(`
-Thank you so much for getting back to me cause this has really been bugging me the last few days and
-normally if this was some person I just met, I would have really just moved on and forgot that they
-existed; but Allan is like family to me so that’s why I’m just trying to figure out what’s going on.
+blbh
 
-My issue with Allan is that he’s become distant ever since 6-10 months ago. I’m definitely not one
-to talk since I’m beyond busy all the time, but we still texted here and there or gamed out; but the
-last few times we spoke, it was facilitated by me; whether I texted the group between him and I with
-David or I texted him about the wedding or birthday. He did reach out to me on my birthday and we
-texted about my grandparents but other than that, we never spoke.
-
-So I texted him the other day to see how’s everyone is doing and I was sharing with Allan that I’m
-really pissed and upset that he never invites me anymore to play or see each other or just a usual
-“how are you” like he always did (something I always complimented him on). I mentioned to him that
-he’s become distant and really left me hanging when he started playing with Andy and Co. and kinda
-dropped me on the sideline. 
-
-In the middle of this, he’s texting me like a robot and instead of understanding where I am coming
-from, he was just saying “thanks for sharing” and ending the convo without addressing anything. Idk
-about you, but when someone is just texting you like a stranger and visibly trying to end the
-conversation, my blood boils… he even mentioned how people have different paths in life and I had no
-idea what that had to do with what we were talking about.
-
-Tbh, in the moment all I saw was red considering everything I’ve done for him and Vice versa and
-instead of going to cursing and telling him off with how he’s texting me and acting, I wanted to
-reach out to you or David and see if he’s maybe going through something or if I potentially did
-something?
+blah bla
 `)
