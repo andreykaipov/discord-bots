f11c3b4870ea0a4a892e891eb9edb46d66e3ffe2
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-12-31 20:51:46 -0500

    deregister commands

--- cf/gochujang/src/register.js
+++ cf/gochujang/src/register.js
@@ -1,12 +1,14 @@
 import * as register from 'lib/register.js'
 
-register.commands([
-    {
-        name: 'ping',
-        description: 'ping pong',
-    },
-    {
-        name: 'thanks',
-        description: 'Thank you so much for getting back to me...',
-    },
-])
+register.commands([])
+
+//register.commands([
+//    {
+//        name: 'ping',
+//        description: 'ping pong',
+//    },
+//    {
+//        name: 'thanks',
+//        description: 'Thank you so much for getting back to me...',
+//    },
+//])
